61c4a70e9cb74d0852a7363e3d82a0100a98ad70
Max Lama <mkl1328@rit.edu>
2023-10-01 22:12:41 -0400

    API 2 Done
    Everything works
    /getUsers returns the users object and not the object that holds the users object

--- src/server.js
+++ src/server.js
@@ -1,29 +1,67 @@
 const http = require('http');
 const url = require('url');
 const query = require('querystring');
-const responseHandler = require('./responses.js');
+
+const htmlHandler = require('./htmlResponses.js');
+const jsonHandler = require('./jsonResponses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const urlStruct = {
-  '/': responseHandler.getIndex,
-  '/style.css': responseHandler.getStyles,
-  notFound: responseHandler.resourceNotFound,
+  GET: {
+    '/': htmlHandler.getIndex,
+    '/style.css': htmlHandler.getStyles,
+    '/getUsers': jsonHandler.getUsers,
+    '/updateUser': jsonHandler.updateUser,
+    notFound: jsonHandler.notFound,
+  },
+  HEAD: {
+    '/getUsers': jsonHandler.getUsersMeta,
+    notFound: jsonHandler.notFoundMeta,
+  },
+  POST: true,
+};
+
+const parseBody = (request, response, handler) => {
+  const body = [];
+  request.on('error', (err) => {
+    console.dir(err);
+    response.statusCode = 400;
+    response.end();
+  });
+
+  request.on('data', (chunk) => {
+    body.push(chunk);
+  });
+
+  request.on('end', () => {
+    const bodyString = Buffer.concat(body).toString();
+    const bodyParams = query.parse(bodyString);
+
+    handler(request, response, bodyParams);
+  });
+};
+
+const handlePost = (request, response, parsedUrl) => {
+  if (parsedUrl.pathname === '/addUser') {
+    parseBody(request, response, jsonHandler.addUser);
+  }
 };
 
 const onRequest = (request, response) => {
   const parsedUrl = url.parse(request.url);
-  const params = query.parse(parsedUrl.query);
-  // Expecting JSON only, so no need for this line
-  // const acceptedTypes = request.headers.accept.split(',')
 
-  if (urlStruct[parsedUrl.pathname]) {
-    urlStruct[parsedUrl.pathname](request, response, params);
-  } else {
-    urlStruct.notFound(request, response, params);
+  if (request.method === 'POST') {
+    return handlePost(request, response, parsedUrl);
   }
+
+  if (urlStruct[request.method][parsedUrl.pathname]) {
+    return urlStruct[request.method][parsedUrl.pathname](request, response);
+  }
+
+  return urlStruct.GET.notFound(request, response);
 };
 
-http.createServer(onRequest).listen(port);
-
-console.log(`Listening on 127.0.0.1:${port}`);
+http.createServer(onRequest).listen(port, () => {
+  console.log(`Listening on 127.0.0.1:${port}`);
+});
